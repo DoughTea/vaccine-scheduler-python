932e0b5c067daee49be5a946ab04ed9607187cea
AndrewDoty <40150180+DoughTea@users.noreply.github.com>
2022-11-30 13:01:51 -0800

    help

---
+++ vaccine-scheduler-python-master/src/main/resources/create.sql
@@ -0,0 +1,25 @@
+CREATE TABLE Caregivers (
+    Username varchar(255),
+    Salt BINARY(16),
+    Hash BINARY(16),
+    PRIMARY KEY (Username)
+);
+
+CREATE TABLE Availabilities (
+    Time date,
+    Username varchar(255) REFERENCES Caregivers,
+    PRIMARY KEY (Time, Username)
+);
+
+CREATE TABLE Vaccines (
+    Name varchar(255),
+    Doses int,
+    PRIMARY KEY (Name)
+);
+
+CREATE TABLE Patients (
+    PUsername varchar(255),
+    Salt BINARY(16),
+    Hash BINARY(16),
+    PRIMARY KEY (PUsername)
+);
